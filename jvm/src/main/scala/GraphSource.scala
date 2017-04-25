8b98e41834cea7a0fc610c1f6509f6e2a5e52db0
Neel Smith <dnsmith.neel@gmail.com>
2017-04-24 23:48:35 -0400

    Test building graph from csv file

--- jvm/src/main/scala/GraphSource.scala
+++ jvm/src/main/scala/GraphSource.scala
@@ -1,7 +1,12 @@
 package edu.holycross.shot.chrongraph
 
+import scala.io.Source
 
 
 object GraphSource {
 
+  def apply(fName: String) : ChronologicalGraph = {
+    val csv = Source.fromFile(fName).getLines.toVector.mkString("\n")
+    GraphFactory.fromCsv(csv)
+  }
 }
--- jvm/src/test/scala/GraphSourceSpec.scala
+++ jvm/src/test/scala/GraphSourceSpec.scala
@@ -4,7 +4,15 @@ import org.scalatest.FlatSpec
 
 class GraphSourceSpec extends FlatSpec {
 
-  "A graph source"  should "create a chronological graph from a CSV file" in  pending
+  "A graph source"  should "create a chronological graph from a CSV file" in  {
+    val f = "jvm/src/test/resources/pm.csv"
+    val g = GraphSource(f)
+    g match {
+      case cg : ChronologicalGraph => assert(true)
+      case _ => fail("Should have created a chronological graph")
+    }
+
+  }
 
 
 
