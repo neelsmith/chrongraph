ac044d6e861370445c7eddfd732be32cf243b866
Neel Smith <dnsmith.neel@gmail.com>
2017-05-03 13:28:17 -0400

    Update test

--- shared/src/test/scala/GraphFactorySpec.scala
+++ shared/src/test/scala/GraphFactorySpec.scala
@@ -10,7 +10,7 @@ import scalax.collection.GraphPredef._
 class GraphFactorySpec extends FlatSpec {
 
   "A graph factory"  should "be able to create a single directed edge from CSV source" in  {
-    val csv = "urn:cts:chronepig:chron.pm:1,urn:cite2:chron:event:2,precedes,epoch,urn:cite2:chron:epoch:pm,1318"
+    val csv = "urn:cts:chronepig:chron:pm:1,urn:cite2:chron:event:2,precedes,epoch,urn:cite2:chron:epoch:pm,1318"
 
     val edge = GraphFactory.edgeFromCsv(csv)
 
@@ -25,7 +25,7 @@ class GraphFactorySpec extends FlatSpec {
     val labelMap = Map("urn:cite2:chron:atticrulers:1" -> "Kingship of Cecrops in Athens",
 "urn:cite2:chron:epoch:pm" -> "Epoch of the Parian Marble" )
 
-   val csv = "urn:cts:chronepig:chron.pm:1,urn:cite2:chron:atticrulers:1,precedes,epoch,urn:cite2:chron:epoch:pm,1318"
+   val csv = "urn:cts:chronepig:chron:pm:1,urn:cite2:chron:atticrulers:1,precedes,epoch,urn:cite2:chron:epoch:pm,1318"
 
    val edge = GraphFactory.edgeFromCsv(csv, labelMap)
 
@@ -40,9 +40,9 @@ class GraphFactorySpec extends FlatSpec {
 
 
   it should "be able to create a directed graph from a multi-line CSV source string" in {
-    val csv = """urn:cts:chronepig:chron.pm:1,urn:cite2:chron:event:2,precedes,epoch,urn:cite2:chron:epoch:pm,1318
-urn:cts:chronepig:chron.pm:2,urn:cite2:chron.event:3,precedes,epoch,urn:cite2:chron:epoch:pm,1310
-urn:cts:chronepig:chron.pm:2,urn:cite2:chron.event:3,contemporary,eponym,urn:cite:chron.atticrulers:1,0
+    val csv = """urn:cts:chronepig:chron:pm:1,urn:cite2:chron:event:2,precedes,epoch,urn:cite2:chron:epoch:pm,1318
+urn:cts:chronepig:chron:pm:2,urn:cite2:chron:event:3,precedes,epoch,urn:cite2:chron:epoch:pm,1310
+urn:cts:chronepig:chron:pm:2,urn:cite2:chron:event:3,contemporary,eponym,urn:cite:chron:atticrulers:1,0
 """
 
     val g = GraphFactory.fromCsv(csv)
@@ -60,7 +60,7 @@ urn:cts:chronepig:chron.pm:2,urn:cite2:chron.event:3,contemporary,eponym,urn:cit
     val csv = """
 
 
-urn:cts:chronepig:chron.pm:1,urn:cite2:chron:atticrulers:1,precedes,epoch,urn:cite2:chron:epoch:pm,1318"""
+urn:cts:chronepig:chron:pm:1,urn:cite2:chron:atticrulers:1,precedes,epoch,urn:cite2:chron:epoch:pm,1318"""
 
     val edge = GraphFactory.edgeFromCsv(csv, labelMap)
 
@@ -72,16 +72,17 @@ urn:cts:chronepig:chron.pm:1,urn:cite2:chron:atticrulers:1,precedes,epoch,urn:ci
       case _ => fail("Should have created a labelled directed edge")
     }
   }
-  it should "make all links bidirectional when importing csv" in  pending /*{
-    val csv = """urn:cts:chronepig:chron.pm:1,urn:cite2:chron:event:2,precedes,epoch,urn:cite2:chron:epoch:pm,1318
-urn:cts:chronepig:chron.pm:2,urn:cite2:chron.event:3,precedes,epoch,urn:cite2:chron:epoch:pm,1310
-urn:cts:chronepig:chron.pm:2,urn:cite2:chron.event:3,contemporary,eponym,urn:cite:chron.atticrulers:1,0
+
+  it should "make all links bidirectional when importing csv" in  {
+    val csv = """urn:cts:chronepig:chron:pm:1,urn:cite2:chron:event:2,precedes,epoch,urn:cite2:chron:epoch:pm,1318
+urn:cts:chronepig:chron:pm:2,urn:cite2:chron:event:3,precedes,epoch,urn:cite2:chron:epoch:pm,1310
+urn:cts:chronepig:chron:pm:2,urn:cite2:chron:event:3,contemporary,eponym,urn:cite:chron:atticrulers:1,0
 """
 
     val g = GraphFactory.fromCsv(csv)
-    //println("BIDI LINKS = " + g.graph)
+    assert (g.graph.edges.size == 6)
 
-  }*/
+  }
 
 
 }
